9a1298366129bb4b77fbd72f66cee5c15328d0f8
SarbaNanda Bhikkhu <sarbanandabhikkhu@gmail.com>
2023-12-30 10:52:12 +0600

    init demo project

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.title}>Expo Examples</Text>
+      <Text style={styles.description}>React Native Expo CLI EAS Build Examples</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ffffff"
+  },
+  title: {
+    fontSize: 48,
+    color: "gold"
+  },
+  description: {
+    marginTop: 10,
+    fontSize: 16,
+    color: "goldenrod"
+  }
+});
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # expo-examples
+
 My Expo React Native EAS Build Examples
+
+```bash
+git clone https://github.com/sarbanandabhikkhu/expo-examples.git
+
+cd expo-examples
+
+npm install -g expo-cli eas-cli
+
+npm install
+
+npm start
+
+eas login
+
+eas build --profile preview
+
+```
